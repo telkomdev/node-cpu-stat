d6b66df573e1ecf0bf4616be8e3dd6d99e4a01a2
Wuriyanto <wuriyanto48@yahoo.co.id>
2019-12-05 21:26:47 +0700

    move iostat to websocket

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const execIOStat = require('./lib/iostat');
+const Websocket = require('ws');
+const express = require('express');
 
-execIOStat().then(out => {
-    console.log(out);
-}).catch(err => {
-    console.log('err ', err);
+// load env
+require('dotenv').config();
+
+const app = express();
+const server = require('http').createServer(app);
+
+const listener = new Websocket.Server({
+    server: server
+});
+
+// init chat
+require('./lib/socket')(listener);
+
+
+// set PORT
+const PORT = process.env.PORT;
+
+// listen app on PORT
+server.listen(PORT, err => {
+    if (err) {
+        console.log(`error on start up: ${err}`);
+    } else {
+        console.log(`app listen on port ${PORT}`);
+    }
 });
